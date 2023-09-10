851f76047eb7177cadb36856a8fece9554e6328d
Huy Lê <huynl.dev@gmail.com>
2023-09-10 18:16:18 +0700

    init source

---
+++ levelLife-api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LevelLife.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
